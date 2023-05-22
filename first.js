ba4bb6408037b70301cbf210656171f06e324878
Rasvom <rasvomb@gmail.com>
2023-05-22 13:20:43 +0300

    export

---
+++ first.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  return a + b;
+}
+
+export const user = {
+  name: "Chubaka",
+  age: 8,
+};
+
+export const year = 2023;
+
+export default sum;
+
+
+// module.exports = {sum, user, year}
---
+++ second.js
@@ -0,0 +1,6 @@
+// const { user, name, sum } = require("./first.js");
+
+import { user, year } from "./first.js";
+import sum from "./first.js";
+console.log(user);
+console.log(sum(1, 2));
